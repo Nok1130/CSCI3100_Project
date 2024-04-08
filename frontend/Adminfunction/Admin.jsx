b4f44d534f2d008d71560a52a839d84ac052ef62
Justine <tszki800@gmail.com>
2024-04-08 17:27:33 +0800

    connected to api

--- frontend/Adminfunction/Admin.jsx
+++ frontend/Adminfunction/Admin.jsx
@@ -1,23 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import "./Admin.css";
 import SideBar from './SideBar.jsx';
+
 import UserMgtPage from './userMgtPage.jsx';
 import PostMgtPage from './PostMgtPage.jsx';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Header from './header.jsx'
 
 function Admin(){
-
-    const getAllUser = async () => {
-        const response = await fetch('http://localhost:5001/api/admin/getAllUser');
-        const data = await response.json();
-        console.log(data);
-    }
-
-    useEffect(() => {
-        getAllUser();
-    }, []);
-
     return ( 
    
      <div className='admin'>
